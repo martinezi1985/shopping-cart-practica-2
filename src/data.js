430671c1e0d86429e245c0cb6301f2fa9c25d26d
Ignacio Gabriel Martinez <martinezi1985@gmail.com>
2022-11-28 00:42:54 -0300

    Practica carrito 2

---
+++ src/data.js
@@ -0,0 +1,25 @@
+let shopItemsData = [{
+  id: 1,
+  name: "Casual Shirt",
+  price: 45,
+  desc: "Camisa casual",
+  img: "images/img-1.jpg"
+},{
+  id: 2,
+  name: "Office Shirt",
+  price: 100,
+  desc: "Camisa oficina",
+  img: "images/img-2.jpg"
+},{
+  id: 3,
+  name: "T Shirt",
+  price: 25,
+  desc: "remera casual",
+  img: "images/img-3.jpg"
+},{
+  id: 4,
+  name: "Mens Suit",
+  price: 300,
+  desc: "Traje para oficina",
+  img: "images/img-4.jpg"
+}]
